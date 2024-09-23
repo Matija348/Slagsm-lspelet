ea8e144776790ddff9a891206acc49e8f8ffb7ee
Matija Zezelj <matija.zezelj@elev.ga.ntig.se>
2024-09-23 08:21:23 +0200

    Fighters namn och hp

--- Slagsmålspelet/Program.cs
+++ Slagsmålspelet/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿{
+    
+    {
+        
+        {
+            // Skapa två slagskämpar med namn och hp
+            Fighter fighterA = new Fighter("Slagskämpe A", 100);
+            Fighter fighterB = new Fighter("Slagskämpe B", 100);
+
+            Random random = new Random();
+
+            // While-loop för att simulera kampen så länge båda har HP kvar
+            while (fighterA.HP > 0 && fighterB.HP > 0)
+            {
+                // Slagskämpe A attackerar B
+                int damageToB = random.Next(5, 16); // Slumpa en skada mellan 5 och 15
+                fighterB.TakeDamage(damageToB);
+                Console.WriteLine($"{fighterA.Name} slår {fighterB.Name} för {damageToB} skada! {fighterB.Name} har {fighterB.HP} HP kvar.");
+
+                // Kontrollera om B är besegrad
+                if (fighterB.HP <= 0) break;
+
+                // Slagskämpe B attackerar A
+                int damageToA = random.Next(5, 16);
+                fighterA.TakeDamage(damageToA);
+                Console.WriteLine($"{fighterB.Name} slår {fighterA.Name} för {damageToA} skada! {fighterA.Name} har {fighterA.HP} HP kvar.");
+
+                // Kontrollera om A är besegrad
+                if (fighterA.HP <= 0) break;
+
+                // Pausa för att göra striden mer spännande
+                System.Threading.Thread.Sleep(1000); // Vänta 1 sekund mellan rundorna
+            }
+
+            // Avgör vinnaren
+            if (fighterA.HP <= 0 && fighterB.HP <= 0)
+            {
+                Console.WriteLine("Det blev oavgjort! Båda slagskämparna föll samtidigt.");
+            }
+            else if (fighterA.HP <= 0)
+            {
+                Console.WriteLine($"{fighterB.Name} vann striden!");
+            }
+            else if (fighterB.HP <= 0)
+            {
+                Console.WriteLine($"{fighterA.Name} vann striden!");
+            }
+        }
+    }
+    
+    
